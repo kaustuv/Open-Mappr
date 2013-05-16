30648f0aaa6a9a79e794c8c389365f2b8a2bda1e
Sundev <sundev@brainvise.com>
2013-05-16 10:27:32 -0600

    fixed length of table

--- mappr_db.sql
+++ mappr_db.sql
@@ -1,17 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 3.5.2
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: May 16, 2013 at 09:59 AM
--- Server version: 5.5.25
--- PHP Version: 5.2.17
+-- Host: internal-db.s67753.gridserver.com
+-- Generation Time: May 16, 2013 at 09:26 AM
+-- Server version: 5.1.63-rel13.4
+-- PHP Version: 5.3.15
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Database: `mappr_v2`
+-- Database: `db67753_mappr`
 --
 
 -- --------------------------------------------------------
@@ -20,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `issues`
 --
 
-CREATE TABLE `issues` (
+CREATE TABLE IF NOT EXISTS `issues` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `projectId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
@@ -38,7 +44,7 @@ CREATE TABLE `issues` (
 -- Table structure for table `issuesIssueTags`
 --
 
-CREATE TABLE `issuesIssueTags` (
+CREATE TABLE IF NOT EXISTS `issuesIssueTags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tagId` int(11) NOT NULL,
   `issueId` int(11) NOT NULL,
@@ -53,7 +59,7 @@ CREATE TABLE `issuesIssueTags` (
 -- Table structure for table `issueTags`
 --
 
-CREATE TABLE `issueTags` (
+CREATE TABLE IF NOT EXISTS `issueTags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
@@ -65,7 +71,7 @@ CREATE TABLE `issueTags` (
 -- Table structure for table `nodes`
 --
 
-CREATE TABLE `nodes` (
+CREATE TABLE IF NOT EXISTS `nodes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `projectId` int(11) NOT NULL,
   `issueInd` int(4) NOT NULL,
@@ -89,7 +95,7 @@ CREATE TABLE `nodes` (
 -- Table structure for table `nodesNodeTags`
 --
 
-CREATE TABLE `nodesNodeTags` (
+CREATE TABLE IF NOT EXISTS `nodesNodeTags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tagId` int(11) NOT NULL,
   `nodeId` int(11) NOT NULL,
@@ -102,7 +108,7 @@ CREATE TABLE `nodesNodeTags` (
 -- Table structure for table `nodeTags`
 --
 
-CREATE TABLE `nodeTags` (
+CREATE TABLE IF NOT EXISTS `nodeTags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
@@ -114,7 +120,7 @@ CREATE TABLE `nodeTags` (
 -- Table structure for table `projects`
 --
 
-CREATE TABLE `projects` (
+CREATE TABLE IF NOT EXISTS `projects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   `url` varchar(64) NOT NULL,
@@ -137,7 +143,7 @@ CREATE TABLE `projects` (
 -- Table structure for table `projectsIssueTags`
 --
 
-CREATE TABLE `projectsIssueTags` (
+CREATE TABLE IF NOT EXISTS `projectsIssueTags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tagId` int(11) NOT NULL,
   `projectId` int(11) NOT NULL,
@@ -152,7 +158,7 @@ CREATE TABLE `projectsIssueTags` (
 -- Table structure for table `projectStates`
 --
 
-CREATE TABLE `projectStates` (
+CREATE TABLE IF NOT EXISTS `projectStates` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   `link` varchar(64) NOT NULL,
@@ -165,7 +171,7 @@ CREATE TABLE `projectStates` (
 -- Table structure for table `userDeletedToNodes`
 --
 
-CREATE TABLE `userDeletedToNodes` (
+CREATE TABLE IF NOT EXISTS `userDeletedToNodes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `projectId` int(11) NOT NULL,
@@ -180,7 +186,7 @@ CREATE TABLE `userDeletedToNodes` (
 -- Table structure for table `userLinks`
 --
 
-CREATE TABLE `userLinks` (
+CREATE TABLE IF NOT EXISTS `userLinks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `projectId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
@@ -202,13 +208,13 @@ CREATE TABLE `userLinks` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(64) NOT NULL,
   `last_name` varchar(64) NOT NULL,
   `user_email` varchar(128) NOT NULL,
   `user_pass` varchar(60) NOT NULL,
-  `user_register_pass` varchar(60) NOT NULL,
+  `user_register_pass` varchar(128) NOT NULL,
   `user_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `user_modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `user_last_login` datetime NOT NULL,
@@ -217,13 +223,14 @@ CREATE TABLE `users` (
   `category` int(11) NOT NULL,
   `notes` text NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=120 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=123 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` VALUES(103, 'First', 'User', 'new@vibrantdatalabs.org', '$P$BGrh4i6vauoa4UiueAtgFbA7kugIz11', 'dCjyUiOK', '2012-12-22 12:03:31', '2012-12-22 12:03:31', '2012-12-22 12:03:31', 1, 1, 0, '');
+INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `user_email`, `user_pass`, `user_register_pass`, `user_date`, `user_modified`, `user_last_login`, `isAdministrator`, `isConfirmed`, `category`, `notes`) VALUES
+(1, 'First', 'User', 'new@vibrantdatalabs.org', '$P$BGrh4i6vauoa4UiueAtgFbA7kugIz11', 'dCjyUiOK', '2012-12-22 12:03:31', '2012-12-22 12:03:31', '2013-05-16 09:18:22', 1, 1, 0, '');
 
 -- --------------------------------------------------------
 
@@ -231,7 +238,7 @@ INSERT INTO `users` VALUES(103, 'First', 'User', 'new@vibrantdatalabs.org', '$P$
 -- Table structure for table `usersChosenFromNodes`
 --
 
-CREATE TABLE `usersChosenFromNodes` (
+CREATE TABLE IF NOT EXISTS `usersChosenFromNodes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `projectId` int(11) NOT NULL,
@@ -248,7 +255,7 @@ CREATE TABLE `usersChosenFromNodes` (
 -- Table structure for table `usersFromNodes`
 --
 
-CREATE TABLE `usersFromNodes` (
+CREATE TABLE IF NOT EXISTS `usersFromNodes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `nodeId` int(11) NOT NULL,
@@ -263,7 +270,7 @@ CREATE TABLE `usersFromNodes` (
 -- Table structure for table `usersProjects`
 --
 
-CREATE TABLE `usersProjects` (
+CREATE TABLE IF NOT EXISTS `usersProjects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
   `projectId` int(11) NOT NULL,
@@ -319,3 +326,7 @@ ALTER TABLE `usersFromNodes`
 ALTER TABLE `usersProjects`
   ADD CONSTRAINT `usersprojects_ibfk_1` FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `usersprojects_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
